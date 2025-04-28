189e64da20ae67f003f4d6e6a4cfe8c292a4cbf1
Vlad-Vasylenko23 <vladyslav.v.vasylenko@ukd.edu.ua>
2025-04-28 22:04:29 +0300

    task 4

---
+++ app_blog/forms.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from django import forms
+from .models import ArticleImage
+
+
+class ArticleImageForm(forms.ModelForm):
+    class Meta:
+        model = ArticleImage
+        fields = ('image', 'title')
