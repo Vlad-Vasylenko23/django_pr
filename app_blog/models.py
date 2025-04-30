28e238e8bf5ce72b0b8c51a2bc07046227a57994
Vlad-Vasylenko23 <vladyslav.v.vasylenko@ukd.edu.ua>
2025-04-30 22:41:47 +0300

    task 5

--- app_blog/models.py
+++ app_blog/models.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
-from django.utils import timezone
 from django.db import models
 from django.urls import reverse
-
+from django.utils import timezone
 
 class Category(models.Model):
-    category = models.CharField(u'Категорія', max_length=250, help_text=u'Максимум 250 символів')
+    category = models.CharField(u'Категорія', max_length=250, help_text=u'Максимум 250 сим.')
     slug = models.SlugField(u'Слаг')
+    objects = models.Manager()
 
     class Meta:
         verbose_name = u'Категорія для публікації'
@@ -15,13 +14,19 @@ class Category(models.Model):
     def __str__(self):
         return self.category
 
+    def get_absolute_url(self):
+        try:
+            url = reverse('articles-category-list', kwargs={'slug': self.slug})
+        except:
+            url = "/"
+        return url
 
 class Article(models.Model):
     title = models.CharField(u'Заголовок', max_length=250, help_text=u'Максимум 250 сим.')
     description = models.TextField(blank=True, verbose_name=u'Опис')
-    pub_date = models.DateTimeField(u'Дата публікації', default=timezone.now)
-    slug = models.SlugField(u'Слаг', unique_for_date='pub_date')
-    main_page = models.BooleanField(u' Головна', default=False, help_text=u'Показувати')
+    pub_date = models.DateTimeField(verbose_name=u'Дата публікації', default=timezone.now)
+    slug = models.SlugField(verbose_name=u'Слаг', unique_for_date='pub_date')
+    main_page = models.BooleanField(u'Головна', default=False, help_text=u'Показувати на головній сторінці')
     category = models.ForeignKey(Category, related_name='news', blank=True, null=True, verbose_name=u'Категорія', on_delete=models.CASCADE)
     objects = models.Manager()
 
@@ -35,12 +40,14 @@ class Article(models.Model):
 
     def get_absolute_url(self):
         try:
-            url = reverse('news-detail', kwargs={'year': self.pub_date.strftime("%Y"), 'month': self.pub_date.strftime("%m"), 'day': self.pub_date.strftime("%d"), 'slug': self.slug, })
+            url = reverse('news-detail', kwargs={'year': self.pub_date.strftime("%Y"),
+                                                'month': self.pub_date.strftime("%m"),
+                                                'day': self.pub_date.strftime("%d"),
+                                                'slug': self.slug})
         except:
             url = "/"
         return url
-
-
+    
 class ArticleImage(models.Model):
     article = models.ForeignKey(Article, verbose_name=u'Стаття', related_name='images', on_delete=models.CASCADE)
     image = models.ImageField(u'Фото', upload_to='photos')
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # URL-адреса для адмін-панелі Django
     path('', include('app_blog.urls')),  # Підключаємо URL-адреси з app_blog
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
